62c1dd9fad71a77d3485bb9f84d2830a9c4f314f
김영진 <kyjjulia07@gmail.com>
2025-04-24 10:39:25 +0900

    :tada: init: 프로젝트 생성

---
+++ android/mobile/src/main/java/com/example/eggi/MyApplication.kt
@@ -0,0 +1,16 @@
+package com.example.eggi
+
+import android.app.Application
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // 또는 단순히 클래스를 참조하여 필요할 때 초기화되도록 할 수도 있음
+        // 실제 사용 시점에 초기화됨
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+    }
+}
---
+++ android/mobile/src/main/java/com/example/eggi/ui/main/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.eggi.ui.main
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.widget.Button
+import com.example.eggi.R
+import com.example.eggi.ui.simple.SimpleActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // 버튼 찾기
+        val btnGoToSimple = findViewById<Button>(R.id.btn_go_to_simple)
+
+        // 클릭 이벤트 설정
+        btnGoToSimple.setOnClickListener {
+            val intent = Intent(this, SimpleActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
