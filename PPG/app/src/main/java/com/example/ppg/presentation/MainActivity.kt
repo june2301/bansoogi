64a0159612f0833a8283cf89d7c884f860cb1ca3
Won, Kee-hoon <lasnier@naver.com>
2025-05-12 09:19:04 +0900

    :sparkles: Feat: 자세 선택 select 구현
    #S12P31A302-84 #time 1h

--- PPG/app/src/main/java/com/example/ppg/presentation/MainActivity.kt
+++ PPG/app/src/main/java/com/example/ppg/presentation/MainActivity.kt
@@ -20,7 +20,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Picker
 import androidx.wear.compose.material.Text
+import androidx.wear.compose.material.rememberPickerState
 import androidx.wear.compose.material.TimeText
 import androidx.wear.tooling.preview.devices.WearDevices
 import com.example.ppg.R
@@ -41,32 +43,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun WearApp(greetingName: String) {
+fun WearApp(appName: String) {
     PPGTheme {
+        val options = listOf(
+            "서 있기 / standing",
+            "바른 자세로 앉기 / upright-sitting",
+            "반듯이 눕기 / supine-lying",
+            "뒤로 기대기 / reclined",
+            "앞으로 숙이기 / forward-lean",
+            "다리를 꼬고 앉기 / cross-legged",
+            "배를 대고 누우기 / prone",
+            "반좌위 / Fowler’s",
+            "오른쪽 옆으로 누우기 / right-lat",
+            "왼쪽 옆으로 누우기 / left-lat"
+        )
+
+        val pickerState = rememberPickerState(
+            initialNumberOfOptions = options.size,
+            initiallySelectedOption = 0
+        )
+
         Box(
             modifier = Modifier
                 .fillMaxSize()
                 .background(MaterialTheme.colors.background),
             contentAlignment = Alignment.Center
         ) {
-            TimeText()
-            Greeting(greetingName = greetingName)
+            TimeText(modifier = Modifier.align(Alignment.TopCenter))
+            Picker(
+                state = pickerState,
+                modifier = Modifier.fillMaxWidth(),
+            ) { index ->
+                Text(
+                    text = options[index],
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    color = MaterialTheme.colors.primary
+                )
+            }
         }
     }
-}
-
-@Composable
-fun Greeting(greetingName: String) {
-    Text(
-        modifier = Modifier.fillMaxWidth(),
-        textAlign = TextAlign.Center,
-        color = MaterialTheme.colors.primary,
-        text = stringResource(R.string.hello_world, greetingName)
-    )
 }
 
 @Preview(device = WearDevices.SMALL_ROUND, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
-    WearApp("Preview Android")
+    WearApp("PPG")
 }
